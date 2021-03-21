e4bc88a11419de37ee532117f2720021e493c84e
Atharva Marathe <69299285+atharvamarathe@users.noreply.github.com>
2021-03-21 11:20:54 +0530

    Rudimentary malloc implementation

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mm.h"
+
+
+typedef struct buf {
+    int a,b,c,d;
+}buf;
+
+int main() {
+
+    // mm_init();
+
+//    printf("VM Page Size = %lu\n",SYSTEM_PAGE_SIZE);
+//    void *addr1 = getPages(1);
+//    void *addr2 = getPages(1);
+//    printf("page 1 = %p, page 2 = %p\n",addr1,addr2);
+
+//    mm_init(&d1);
+//    printf("Verifying that block is free %d \n",d1 ->metaData.isFree);
+    buf *a;
+    a= (buf*)Malloc(sizeof(buf));
+    if(a == NULL)
+            printf("Sorry amlloc didnt work\n");
+    a->a = 1;
+    a->b =2;
+    a->c=3;
+    a->d=4;
+    printf("The number is %d\n",a->d);
+    buf *d;
+    d=(buf*)Malloc(sizeof(buf));
+    d->a= 24;
+    d->b=567;
+    d->c=444;
+    d->d=2222;
+    printf("the value of the struct is %d %d %d and %d\n",d->a,d->b,d->c,d->d);
+
+    return 0;
+ }
+
+
+
+
---
+++ mm.h
@@ -0,0 +1,66 @@
+#ifndef MM_H
+#define MM_H
+
+#include <stdint.h>
+
+#define TRUE 1
+#define FALSE 0
+#define MAX_PAGES 5
+
+typedef struct meta_data_block_ {
+
+    int isFree;
+    uint32_t blockSize;
+    struct meta_data_block_ *prevBlock;
+    struct meta_data_block_ *nextBlock;
+    uint32_t offset;
+}meta_data_block_;
+
+typedef meta_data_block_ * meta_data_block;
+
+
+typedef struct data_block_ {
+
+    struct data_block_ *next;
+    struct data_block_ *prev;
+    char *data;
+    meta_data_block metaData;
+}data_block_;
+
+typedef data_block_ * data_block;
+
+// typedef struct page_list{
+
+//     meta_data_block head;
+//     struct page_list_ *next;
+//     struct page_list_ *prev;
+
+// }page_list;
+
+typedef struct page_list {
+
+    meta_data_block *head;
+    uint32_t avaiableSize;
+}page_list;
+
+// void mm_init(data_block *d1);
+int isPageEmpty(meta_data_block head);
+void * getPages(int units);
+void freePages(void* vm_page,int units);
+void mmInit();
+void initPageList();
+void* Malloc(uint32_t size);
+void createBinsList(meta_data_block *head);
+int splitBins(meta_data_block m1,int size);
+int mergeBins(meta_data_block m1);
+meta_data_block getFreeBlock(meta_data_block head);
+// void freeDatablock(data_block d1);
+// void pageInit(data_block *d1);
+static data_block d1;
+static page_list pageList[MAX_PAGES];
+static size_t SYSTEM_PAGE_SIZE;
+static int pageCount=0;
+static int currentFreePage=0;
+
+
+#endif
