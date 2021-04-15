52b0ab5bd305e226b0513c61422e201a62c4a7e6
Atharva Marathe <69299285+atharvamarathe@users.noreply.github.com>
2021-04-15 14:37:08 +0530

    Added comments to largeAlloc

--- largealloc.h
+++ largealloc.h
@@ -8,6 +8,12 @@
 
 #define MAX_SIZE_FOR_LARGE_ALLOC 100
 
+/*
+ * structure of _large_alloc : 
+ * Members : ptr    : pointer to the large memory block 
+ *           offset : location of the memory block from the starting address of the memory page
+ *    remainingSize : the remaining size in the memory page which is avaiable for use .
+ */
 typedef struct _large_alloc {
 
     meta_data_block ptr;
@@ -15,26 +21,28 @@ typedef struct _large_alloc {
     int remainingSize;
 }large_alloc;
 
+/* 
+ * Structure of largeAllocList :
+ * Members : largeBlock : Array of the struct _large_Alloc which will hold the pointers to the free locations
+ *           count      : Position upto which pages are allocated in the array.
+ */
 typedef struct _large_alloc_list {
     large_alloc largeBlock[MAX_SIZE_FOR_LARGE_ALLOC];
     int count;
 }large_alloc_list;
 extern large_alloc_list largeAllocList;
+
 int splitLargeBlock(meta_data_block mptr, size_t bytes);
+
 int mergeLargeBlock(meta_data_block m1);
+
 void* returnLargeBlock(size_t bytes);
+
 void LargeAllocInit();
+
 int isLargeAllocPageEmpty(meta_data_block head);
+
 void removeLargeAllocPage(meta_data_block head);
-
-
-
-
-
-
-
-
-
 
 
 #endif
