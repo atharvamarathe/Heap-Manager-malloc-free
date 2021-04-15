36643d29e6bdedfb0948e1332b11f00d350714d0
Atharva Marathe <69299285+atharvamarathe@users.noreply.github.com>
2021-04-15 14:34:57 +0530

    Added comments to sizeclassfreelist

--- sizeclassfreelist.h
+++ sizeclassfreelist.h
@@ -4,12 +4,28 @@
 #include "sizeclasses.h"
 
 #define SIZECLASS_FREE_LIST_SIZE 100
+
+
+/*
+ * Structure of _sizeclassFreeList :
+ * Members : blockPtr    : Pointer to the memory block
+ *           blockSize   : Size of the memory block
+ *           offset      : The page number where the memory block is stored.
+ */
+
 typedef struct _sizeClass_free_list {
 
     meta_data_block blockPtr;
     int blockSize;
     int offset;
 }_sizeClass_free_list;
+
+
+/*
+ * Structure of sizeClassFreeList :
+ * Members : l    : array of struct _sizeclass_free_list which contains pointers to the memory block of a size-class
+ *           rear : position of the last element in the array (just like a queue)
+ */
 
 typedef struct sizeClass_free_list {
 
@@ -20,10 +36,14 @@ typedef struct sizeClass_free_list {
 extern sizeClass_free_list sizeClassFreeList[NUM_OF_CLASSES];
 
 void initSizeClassFreeList();
-// void addBlocktoSizeClassFreeList(meta_data_block ptr, int size);
+
 meta_data_block getFreeBlockfromFreeList(size_t bytes);
+
 void addBlocktoSizeClassFreeList(meta_data_block ptr, int size,int offset);
+
 int isSizeClassFreeListEmpty(int sizeclass);
+
 void removeBlockfromSizeClassFreeList(meta_data_block ptr, int size);
+
 void removeAllFreeListBlocksFromOffset(int sizeclass, int offset);
 #endif
