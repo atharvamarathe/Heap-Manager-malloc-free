06c2fa714d2af68abc96b39e301ce95eb858beb2
Atharva Marathe <69299285+atharvamarathe@users.noreply.github.com>
2021-04-15 14:40:07 +0530

    Added comments to freelist

--- freelist.h
+++ freelist.h
@@ -1,7 +1,6 @@
 #ifndef FREELIST_H
 #define FREELIST_H
 
-// #include "sizeclasses.h"
 #include "largealloc.h"
 
 #define FREE_LIST_SIZE 100
@@ -9,11 +8,24 @@
 #define GETRCHILD(i) (2*i+2)
 #define GETPARENT(i) ((i-1)/2)
 
+
+/*
+ * structure of _free_list :
+ * Members : blockPtr  : The pointer to the memory block.
+ *           blockSize : The size of the memory block.
+ */
 typedef struct _free_list {
 
     meta_data_block blockPtr;
     int blockSize;
 }_free_list;
+
+/*
+ * Structure of freeList : Customized heap data structure for the implementation fo freelist.
+ * Members : list : array of the struct _free_list
+ *           rear : Pointing to the last element of the list 
+ *           size : Maximum capacity of the heap array
+ */
 
 typedef struct free_list {
  
@@ -24,13 +36,19 @@ typedef struct free_list {
 }free_list;
 
 extern free_list freeList;
-// static free_list ClassFreeList[NUM_OF_CLASSES][];7
+
 void initFreeList(free_list *l1);
+
 void swapListElements(free_list *l1,int i,int j);
+
 int addBlocktoFreeList(free_list *l1,meta_data_block ptr);
+
 void removeBlockfromFreeList(free_list *l1);
+
 void heapifyFreeList();
+
 int deleteBlockfromFreeList(free_list *l1, int size, meta_data_block m1);
+
 int sortFreeList(free_list *l1);
 
 
